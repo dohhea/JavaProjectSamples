d935aa3f490b394e7b5415572473f4cd54469937
Dongik Oh <dohhea@gmail.com>
2022-01-04 12:26:31 +0900

    Boolean return 문장의 조정

--- 모기피하기게임/src/Shape.java
+++ 모기피하기게임/src/Shape.java
@@ -74,8 +74,7 @@ public class Shape extends ImageIcon {
 	// 하나의 점이 이 모양과 충돌하였는지 (모양의 margin 거리안에 있는지)를 판단하는 함수
 	public boolean collide (Point p2) {
 		Point p = new Point(this.x, this.y);
-		if (p.distance(p2) <= margin) return true;
-		return false;
+		return (p.distance(p2) <= margin);			// 함축적으로 표현
 	}
 	
 	public void reset() {
